3461e920def5465dbf479942f745c89c288f47ef
Samantha Curatolo <85077075+scuratolo26@users.noreply.github.com>
2022-08-23 19:24:34 -0400

    connect database

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mysql = require('mysql2');
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MYSQL username, 
+        user: 'root',
+        // Your MYSQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    });
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
